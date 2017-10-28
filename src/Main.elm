282eca75dbd014dbf25e9aaad11e549bb6e85614
Jason Mossburg <mossburg.jason@gmail.com>
2017-10-27 23:44:18 -0500

    elm cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,24 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (Html, Attribute, beginnerProgram, text, div, input)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String
 
 
-main = Html.text "Coming Soon!"
+main = view
+
+
+view : Html.Html msg
+view =
+    div [ myStyle ] [ text "Coming Soon!" ]
+
+
+myStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "40px")
+    , ("padding", "10px 0")
+    , ("font-size", "2em")
+    , ("text-align", "center")
+    ]
